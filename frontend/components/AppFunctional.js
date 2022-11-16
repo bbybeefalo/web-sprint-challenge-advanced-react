14bc5a1e876259eb4e962e0a3ef114d56249fc6d
Morgan Frotten <mmfrotten@gmail.com>
2022-11-16 10:52:49 -0500

    messages

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -8,17 +8,19 @@ const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
+
 export default function AppFunctional(props) {
   const [activeSq, setActiveSq] = useState(initialIndex);
   const [steps, setSteps] = useState(initialSteps);
   const [message, setMessage] = useState(initialMessage);
   const [email, setEmail] = useState(initialEmail);
 
-  function countSteps () {
+
+  function countSteps() {
     setSteps(steps + 1);
   }
 
-  function reset (evt) {
+  function reset(evt) {
     evt.preventDefault();
     setSteps(initialSteps);
     setActiveSq(initialIndex);
@@ -27,55 +29,69 @@ export default function AppFunctional(props) {
   }
 
 
-  function move (evt) {
-    let direction = evt.target.id; 
+  function move(evt) {
+    let direction = evt.target.id;
     if (direction === "down" && activeSq < 6) {
       setActiveSq(activeSq + 3);
       countSteps();
+      setMessage(initialMessage)
+    } else if (direction === "down" && activeSq > 6) {
+      setMessage(`You can't go down`);
+
     } else if (direction === "up" && activeSq > 2) {
       setActiveSq(activeSq - 3);
       countSteps();
-    } else if (direction === "left" && activeSq !== 0 && activeSq !== 3 && activeSq!== 6) {
+      setMessage(initialMessage);
+    } else if (direction === "up" && activeSq < 2) {
+      setMessage(`You can't go up.`);
+
+    } else if (direction === "left" && activeSq !== 0 && activeSq !== 3 && activeSq !== 6) {
       setActiveSq(activeSq - 1);
       countSteps();
-    } else if (direction === "right" && activeSq !== 2 && activeSq !== 5 && activeSq!== 8) {
-      setActiveSq(activeSq + 1);
-      countSteps();
-    }
-    console.log(activeSq)
-  };
+      setMessage(initialMessage);
+    } else if (direction === "left" && activeSq === 0 || activeSq === 3 || activeSq === 6) {
+      setMessage(`You can't go left.`);
 
-  return (
-    <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved {steps} times</h3>
-      </div>
-      <div id="grid">
-        {
-          [0, 1, 2, 
-           3, 4, 5,
-           6, 7, 8].map(idx => (
+    } else if (direction === "right" && activeSq !== 2 && activeSq !== 5 && activeSq !== 8) {
+    setActiveSq(activeSq + 1);
+    countSteps();
+    setMessage(initialMessage);
+    } else if (direction === "right" && activeSq === 2 || activeSq === 5 || activeSq === 8) {
+    setMessage(`You can't go right.`);
+    }
+};
+
+return (
+  <div id="wrapper" className={props.className}>
+    <div className="info">
+      <h3 id="coordinates">Coordinates (2, 2)</h3>
+      <h3 id="steps">You moved {steps} times</h3>
+    </div>
+    <div id="grid">
+      {
+        [0, 1, 2,
+          3, 4, 5,
+          6, 7, 8].map(idx => (
             <div key={idx} className={`square${idx === activeSq ? ' active' : ''}`}>
               {idx === activeSq ? 'B' : null}
             </div>
           ))
-        }
-      </div>
-      <div className="info">
-        <h3 id="message"></h3>
-      </div>
-      <div id="keypad">
-        <button id="left" onClick={move}>LEFT</button>
-        <button id="up" onClick={move}>UP</button>
-        <button id="right" onClick={move}>RIGHT</button>
-        <button id="down" onClick={move}>DOWN</button>
-        <button id="reset" onClick={reset}>reset</button>
-      </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
-      </form>
+      }
     </div>
-  )
+    <div className="info">
+      <h3 id="message">{message}</h3>
+    </div>
+    <div id="keypad">
+      <button id="left" onClick={move}>LEFT</button>
+      <button id="up" onClick={move}>UP</button>
+      <button id="right" onClick={move}>RIGHT</button>
+      <button id="down" onClick={move}>DOWN</button>
+      <button id="reset" onClick={reset}>reset</button>
+    </div>
+    <form>
+      <input id="email" type="email" placeholder="type email"></input>
+      <input id="submit" type="submit"></input>
+    </form>
+  </div>
+)
 }
